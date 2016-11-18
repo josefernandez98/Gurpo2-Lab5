f0ffc72ef68ad1e9068f785f5f98f59c65e21912
Jose Fernandez <jrfo_04@unitec.edu>
2016-11-18 17:04:46 -0600

    Ese harold

--- Main.cpp
+++ Main.cpp
@@ -64,7 +64,6 @@ int main(int argc, char const *argv[]) {
           string nombreCompleto="";
           string numeroTelefono="";
           string direccionCorreo="";
-
           cout << "Ingrese el Nombre Completo: ";
           cin.ignore();
           getline(cin,nombreCompleto);
@@ -81,26 +80,127 @@ int main(int argc, char const *argv[]) {
           cin.ignore();
           getline(cin,fecha);
           cout << endl;
-
-
-
-        }
+          lista.push_back(new Pareja(nombreCompleto,numeroTelefono,direccionCorreo,fecha));
+        }//Fin del if opcion 2
         if(opcion2==3){ //companero trabajo
-
-
-
+            string departamento="";
+            string nombreCompleto="";
+            string numeroTelefono="";
+            string direccionCorreo="";
+            cout << "Ingrese el Nombre Completo: ";
+            cin.ignore();
+            getline(cin,nombreCompleto);
+            cout << endl;
+            cout << "Ingrese el Numero de Telefono: ";
+            cin.ignore();
+            getline(cin,numeroTelefono);
+            cout << endl;
+            cout << "Ingrese la direccion de Correo: ";
+            cin.ignore();
+            getline(cin,direccionCorreo);
+            cout << endl;
+            cout << "Ingrese el departamento en el que trabajan: ";
+            cin.ignore();
+            getline(cin,departamento);
+            cout << endl;
+            lista.push_back(new CompaneroTrabajo(nombreCompleto,numeroTelefono,direccionCorreo, departamento));
         }
         if(opcion2==4){ //companero clase
-
+            string clase="";
+            string nombreCompleto="";
+            string numeroTelefono="";
+            string direccionCorreo="";
+            cout << "Ingrese el Nombre Completo: ";
+            cin.ignore();
+            getline(cin,nombreCompleto);
+            cout << endl;
+            cout << "Ingrese el Numero de Telefono: ";
+            cin.ignore();
+            getline(cin,numeroTelefono);
+            cout << endl;
+            cout << "Ingrese la direccion de Correo: ";
+            cin.ignore();
+            getline(cin,direccionCorreo);
+            cout << endl;
+            cout << "Ingrese la clase en la que se conocieron: ";
+            cin.ignore();
+            getline(cin,clase);
+            cout << endl;
+            lista.push_back(new CompaneroClase(nombreCompleto,numeroTelefono,direccionCorreo,clase));
         }
         if(opcion2==5){ //familiares
-
+            string relacionFamiliar="";
+            string nombreCompleto="";
+            string numeroTelefono="";
+            string direccionCorreo="";
+            cout << "Ingrese el Nombre Completo: ";
+            cin.ignore();
+            getline(cin,nombreCompleto);
+            cout << endl;
+            cout << "Ingrese el Numero de Telefono: ";
+            cin.ignore();
+            getline(cin,numeroTelefono);
+            cout << endl;
+            cout << "Ingrese la direccion de Correo: ";
+            cin.ignore();
+            getline(cin,direccionCorreo);
+            cout << endl;
+            cout << "Ingrese la relacion familiar: ";
+            cin.ignore();
+            getline(cin,relacionFamiliar);
+            cout << endl;
+            lista.push_back(new Familiares(nombreCompleto,numeroTelefono,direccionCorreo,relacionFamiliar));
         }
         if(opcion2==6){ //amantes
-
+            string horarioDisponible="";
+            string nombreCompleto="";
+            string numeroTelefono="";
+            string direccionCorreo="";
+            cout << "Ingrese el Nombre Completo: ";
+            cin.ignore();
+            getline(cin,nombreCompleto);
+            cout << endl;
+            cout << "Ingrese el Numero de Telefono: ";
+            cin.ignore();
+            getline(cin,numeroTelefono);
+            cout << endl;
+            cout << "Ingrese la direccion de Correo: ";
+            cin.ignore();
+            getline(cin,direccionCorreo);
+            cout << endl;
+            cout << "Ingrese el horario disponible: ";
+            cin.ignore();
+            getline(cin,horarioDisponible);
+            cout << endl;
+            lista.push_back(new Amantes(nombreCompleto,numeroTelefono,direccionCorreo,horarioDisponible));
         }
         if(opcion2==7){ //bloqueados
-
+            int nivelOdio=0;
+            string nombreCompleto="";
+            string numeroTelefono="";
+            string direccionCorreo="";
+            cout << "Ingrese el Nombre Completo: ";
+            cin.ignore();
+            getline(cin,nombreCompleto);
+            cout << endl;
+            cout << "Ingrese el Numero de Telefono: ";
+            cin.ignore();
+            getline(cin,numeroTelefono);
+            cout << endl;
+            cout << "Ingrese la direccion de Correo: ";
+            cin.ignore();
+            getline(cin,direccionCorreo);
+            cout << endl;
+            cout << "Ingrese el nivel de odio (1-100): ";
+            cin >> nivelOdio;
+            for (;(nivelOdio<1) || (nivelOdio>100) ;) {
+              cout << "Ingreso un valor invalido" << endl;
+              cout << "Ingrese el nivel de odio (1-100): ";
+              cin >> nivelOdio;
+              cout << endl;
+            }
+            cout << endl;
+            lista.push_back(new Bloqueados(nombreCompleto,numeroTelefono,direccionCorreo,nivelOdio));
         }
         if(opcion2==8){ //regresar
           cout << endl;
